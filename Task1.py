52290031bb95bd019737c726fb18a0dcee81b87b
Amit singh <amitwashere9@gmail.com>
2025-10-11 03:41:06 +0530

    new files

---
+++ Task1.py
@@ -0,0 +1,18 @@
+#Take a sentence as input and decide if it’s positive, negative, or neutral based on words/emojis. 
+
+# String -> I am Amit . I Love coding and I feel very HAPPY while solving problems 😍 , I am a part of Geek room in AI department 😍 , I love being a part of it , I hate only one thing "Theory" it just feels boring 😫 but again I believe the thing which makes you sad makes you happy in long run.
+
+count = 0
+sentence = input("Enter a sentence with emojis: ") 
+mood_dict = {"love": 1, "😍": 1, "happy": 1, "hate": -1, "😫": -1, "sad": -1} 
+sent_low=sentence.lower()
+for word in sent_low.split(" "):
+    if  word in mood_dict:
+            count = count + mood_dict[word]
+print(count)
+if count>0:
+    print("The overall mood is Positive")
+elif count<0:
+    print("The overall mood is Negative")
+else :
+    print("The overall mood is Neutral")
---
+++ Task2.py
@@ -0,0 +1,1 @@
+#Generate a random 2-line funny story by mixing random characters, places, and actions.
