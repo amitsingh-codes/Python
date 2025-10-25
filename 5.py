8daa5e9a4ac55b9318c2941e90cb22967489fa8a
Amit singh <amitwashere9@gmail.com>
2025-10-25 12:26:52 +0530

    task 5 done

--- 5.py
+++ 5.py
@@ -1,18 +1,23 @@
-temp = [30, 31, 33, 34, 35] 
-i=1
-temp_diff=[]
-while i <= len(temp)-1:
-    a = temp[i]-temp[i-1]
-    temp_diff.append(a)
-    i=i+1
-avg_diff = sum(temp_diff)/len(temp)
-print(f"Average temperature difference : {avg_diff}")
-if avg_diff>0:
-    print("The temperature is rising")
-elif avg_diff<0:
-    print("The temperature is falling")
-else:
-    print("The temperature is stable")
+#Given a list of numbers (like daily temperatures or stock prices), detect if the trend is rising, falling, or stable — and estimate the next value.
 
-next_temp = temp[-1] + avg_diff 
-print(f"Predicted next value: {next_temp}")
+def temp_predict(temp):
+    i=1
+    temp_diff=[]
+    while i <= len(temp)-1:
+        a = temp[i]-temp[i-1]
+        temp_diff.append(a)
+        i=i+1
+    avg_diff = sum(temp_diff)/len(temp)
+    print(f"Average temperature difference : {avg_diff}")
+    if avg_diff>0:
+        print("The temperature is rising")
+    elif avg_diff<0:
+        print("The temperature is falling")
+    else:
+        print("The temperature is stable")
+    next_temp = temp[-1] + avg_diff 
+    print(f"Predicted next value: {next_temp}")
+
+
+temp = [31,31,29,28,30,31,29]    
+temp_predict(temp)
