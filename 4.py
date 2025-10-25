c4ad46b21e8d9d96f60c84f3140679904163d688
Amit singh <amitwashere9@gmail.com>
2025-10-25 12:14:59 +0530

    task 4 done

--- 4.py
+++ 4.py
@@ -1,20 +1,23 @@
 #Take a paragraph and show which words appear most often.
-sentence = input("Enter the sentence lil bro :")
-a = sentence.lower()
-list = []
-occur = []
-count = 0
-for word in a.split():
-    if word not in list:
-        for i in a.split():
-            if i == word:
-                count = count + 1
-        occur.append(count)
-        list.append(word)
-        count = 0
-    else:
-        continue
-occurance=max(occur)
-index = occur.index(occurance)
-maxoccur = list[index]
-print(f"The most occuring word in the Sentence is {maxoccur} which have repeated {occurance} times")
+def most_occur(sentence):
+    a = sentence.lower()
+    list = []
+    occur = []
+    count = 0
+    for word in a.split():
+        if word not in list:
+            for i in a.split():
+                if i == word:
+                    count = count + 1
+            occur.append(count)
+            list.append(word)
+            count = 0
+        else:
+            continue
+    occurance=max(occur)
+    index = occur.index(occurance)
+    maxoccur = list[index]
+    print(f"The most occuring word in the Sentence is {maxoccur} which have repeated {occurance} times")
+    
+sentence = "Hello i am amit i i i i i i "
+most_occur(sentence)
