646c9043d0d38297b3ee0180a4ba658ac24ae49f
Amit singh <amitwashere9@gmail.com>
2025-10-27 07:23:27 +0530

    made the code more advance

--- 4.py
+++ 4.py
@@ -1,23 +1,38 @@
 #Take a paragraph and show which words appear most often.
-def most_occur(sentence):
-    a = sentence.lower()
-    list = []
-    occur = []
-    count = 0
-    for word in a.split():
-        if word not in list:
-            for i in a.split():
-                if i == word:
-                    count = count + 1
-            occur.append(count)
-            list.append(word)
-            count = 0
-        else:
-            continue
-    occurance=max(occur)
-    index = occur.index(occurance)
-    maxoccur = list[index]
-    print(f"The most occuring word in the Sentence is {maxoccur} which have repeated {occurance} times")
-    
-sentence = "Hello i am amit i i i i i i "
-most_occur(sentence)
+sentence = input("Enter the sentence lil bro :")
+a = sentence.lower()
+b=list(a)
+c=list(a)
+i = 0
+while i<=len(c):
+    try:
+        if "." in b :
+            b.remove(".")
+        elif "," in b:
+            b.remove(",")
+        elif("!") in b:
+            b.remove("!")
+        elif("?") in b:
+            b.remove("?")
+    except ValueError:
+        pass
+    i+=1
+a = ''.join(b)
+b = a.split()
+list1 = []
+occur = []
+count = 0
+for word in a.split():
+    if word not in list1:
+        for i in a.split():
+            if i == word:
+                count = count + 1
+        occur.append(count)
+        list1.append(word)
+        count = 0
+    else:
+        continue
+occurance=max(occur)
+index = occur.index(occurance)
+maxoccur = list1[index]
+print(f"The most occuring word in the Sentence is {maxoccur} which have repeated {occurance} times")
